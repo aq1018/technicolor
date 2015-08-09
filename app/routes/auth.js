d25623c382bd5529a088ad204e71b5354b2c1f43
Aaron Qian <aq1018@gmail.com>
2015-08-08 18:50:40 -0700

    implement auth route handler

---
+++ app/routes/auth.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = function (users, auth) {
+  // WARNING: VERY INSECURE!!! DO NOT USE THIS!!!
+  //
+  // This endpoint accepts a JSON request formatted below and returns a JSON
+  // response indicating if the user is authenticated.
+  //
+  // Example Request:
+  //   {
+  //     "username": "<username>",
+  //     "password": "<password>"
+  //    }
+  //
+  // Example Response:
+  //   {
+  //     "authenticated": true
+  //   }
+  //
+  function authenticateUserEndpoint(req, res, next) {
+    users
+
+      // get the user from db based on supplied user name
+      .getByUsername(req.params.username)
+
+      // authenticate user returns true / false
+      .then(function (user) {
+        if(!user) { return false; }
+        return auth.passwordMatch(user.passwordHash, req.params.password);
+      })
+
+      // render json response
+      .then(function(authenticated) {
+        res.send({authenticated: authenticated});
+      })
+
+      // error handling and promise chain termination
+      .nodeify(next);
+  }
+
+  return authenticateUserEndpoint;
+};
+
+module.exports['@require'] = ['repositories/users', 'services/auth'];
+module.exports['@singleton'] = true;
---
+++ spec/unit/routes/auth.test.js
@@ -0,0 +1,86 @@
+/* globals describe, it, expect, before, after, ioc */
+
+'use strict';
+
+require('../helper');
+
+var Q = require('q');
+var httpMocks = require('node-mocks-http');
+var mongo = ioc.create('mongo');
+var handler = ioc.create('routes/auth');
+var fixtures = require('../support/fixtures');
+
+describe('routes', function() {
+  describe('auth', function() {
+
+    // the original password for this hash is 'changeme'
+    var hash = '$2a$10$t2x5pNgPL1RYAKi/pF9Z2eLaoAC.dYRS67LfPfOUMwhnf6GNOiDVq';
+
+    var records = [{
+      username: 'alice',
+      passwordHash: hash
+    }];
+
+    before(function(done) {
+      fixtures.populate('users', records).nodeify(done);
+    });
+
+    after(function(done) {
+      fixtures.truncate('users').nodeify(done);
+    });
+
+    it('authenticates with valid credentical', function(done) {
+      var req = httpMocks.createRequest({
+        method: 'POST',
+        url: '/auth',
+        params: {
+          username: 'alice',
+          password: 'changeme'
+        }
+      });
+
+      var res = httpMocks.createResponse();
+
+      Q.nfcall(handler, req, res).then(function() {
+        var data = res._getData();
+        expect(data.authenticated).to.be.true;
+      }).nodeify(done);
+    });
+
+    it('does not authenticate with invalid password', function(done) {
+      var req = httpMocks.createRequest({
+        method: 'POST',
+        url: '/auth',
+        params: {
+          username: 'alice',
+          password: 'wrong password'
+        }
+      });
+
+      var res = httpMocks.createResponse();
+
+      Q.nfcall(handler, req, res).then(function() {
+        var data = res._getData();
+        expect(data.authenticated).to.be.false;
+      }).nodeify(done);
+    });
+
+    it('does not authenticate with invalid username', function(done) {
+      var req = httpMocks.createRequest({
+        method: 'POST',
+        url: '/auth',
+        params: {
+          username: 'i dont exist',
+          password: 'changeme'
+        }
+      });
+
+      var res = httpMocks.createResponse();
+
+      Q.nfcall(handler, req, res).then(function() {
+        var data = res._getData();
+        expect(data.authenticated).to.be.false;
+      }).nodeify(done);
+    });
+  });
+});
